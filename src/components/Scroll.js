d8dcfb9deb89d42d712adcc560092bb1fb1594eb
Valentin <cassusval@gmail.com>
2020-11-23 17:55:03 +0100

    Changing error in cardlist

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Scroll = ({children}) => {
     return (
-        <div style={{overflowY: 'scroll', border: '3px solid black', height: '700px'}}>
+        <div>
             {children}
         </div>
     )
